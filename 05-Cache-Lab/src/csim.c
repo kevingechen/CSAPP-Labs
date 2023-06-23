f9495dfc1359a5e320617b57dd012a8e44d6889f
Ge CHEN (Kevin) <kevin.ge.chen@gmail.com>
2023-06-23 05:05:35 +0000

    feat(cache): read and parse trace file to linked list of memory access node

--- 05-Cache-Lab/src/csim.c
+++ 05-Cache-Lab/src/csim.c
@@ -1,16 +1,70 @@
 #include "cachelab.h"
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <getopt.h>
+#include <string.h>
 
-unsigned int str2Unsigned(char *str) {
-    unsigned int num = 0;
-    char *char_ptr = str;
-    while ('\0' != *char_ptr) {
-        num = num * 10 + ((*char_ptr) - '0');
-        char_ptr++;
+#define MAX_TRACE_LEN 100
+#define MAX_BEHAVIOR_LEN_PER_MEM_ACCESS 3
+
+typedef enum {
+    UNKOWN = 0,
+    HIT,
+    MISS,
+    EVICTION
+} CacheBehavior;
+
+typedef struct mem_access {
+    char type;
+    char addr[10];
+    char  bsize[5];
+    unsigned int cb_len;
+    CacheBehavior cb_arr[3];
+    struct mem_access *next;
+} mem_access_node;
+
+/**
+ * Parse line by line from trace_file,
+ * build a memory access struct for each valid line.
+ * A valid line is of pattern " {type} {addr_in_hex},{byte_size}"
+ * Return the head pointer of a linked list of mem_access_node
+ */
+mem_access_node* readMemAccessHistoryFromTraceFile(const char *file_name) {
+    mem_access_node* head = NULL;
+    mem_access_node* tail = NULL;
+    FILE* in_file = fopen(file_name, "r");
+    if (in_file == NULL) {
+        printf("Failed to open the file.\n");
+        return head;
     }
-    return num;
+
+    char line[MAX_TRACE_LEN];
+    while (fgets(line, MAX_TRACE_LEN, in_file) != NULL) {
+        // Skip lines for trace of instruction type 'I'
+        if (line[0] != ' ') continue;
+        mem_access_node* current_node = (mem_access_node*) malloc(sizeof(mem_access_node));
+        memset(current_node, 0, sizeof(mem_access_node));  // Initialize addr with zeros
+        sscanf(line, " %c %[^,],%s", &current_node->type, current_node->addr, current_node->bsize);
+        if (NULL == head) {
+            head = current_node;
+            tail = current_node;
+        } else {
+            tail->next = current_node;
+            tail = current_node;
+        }
+    }
+
+    fclose(in_file);
+    return head;
+}
+
+void printMemAccessList(mem_access_node *head) {
+    mem_access_node* move = head;
+    printf("\nThe sequence of memory access:\n");
+    while (move) {
+        printf("\ttype=%c, addr=%s, bsize=%s, cb_len=%d\n", move->type, move->addr, move->bsize, move->cb_len);
+        move = move->next;
+    }
 }
 
 int main(int argc, char *argv[])
@@ -31,13 +85,13 @@ int main(int argc, char *argv[])
                 verbose_flag = 1;
                 break;
             case 's':
-                s = str2Unsigned(optarg);
+                s = atoi(optarg);
                 break;
             case 'E':
-                E = str2Unsigned(optarg);
+                E = atoi(optarg);
                 break;
             case 'b':
-                b = str2Unsigned(optarg);
+                b = atoi(optarg);
                 break;
             case 't':
                 trace_file = optarg;
@@ -48,16 +102,21 @@ int main(int argc, char *argv[])
         }
     }
 
+    mem_access_node *mlist = readMemAccessHistoryFromTraceFile(trace_file);
+
     if (help_flag)
         printf("Usage: %s [-hv] -s <s> -E <E> -b <b> -t <tracefile>\n", argv[0]);
-    if (verbose_flag)
+    if (verbose_flag) {
         // print detailed cache actions for each memory access
         printf("verbose_flag = %d\n", verbose_flag);
-    printf("s = %d, E = %d, b = %d, trace_file = %s\n", s, E, b, trace_file);
+        printMemAccessList(mlist);
+        printf("s = %d, E = %d, b = %d, trace_file = %s\n", s, E, b, trace_file);
+        printf("hit_count = %d, miss_count = %d, eviction_count = %d\n", hit_count, miss_count, eviction_count);
+    }
+
     // parse trace file
     // evaluate cache actions
     // calculate statistics
-    printf("hit_count = %d, miss_count = %d, eviction_count = %d\n", hit_count, miss_count, eviction_count);
     // printSummary(hit_count, miss_count, eviction_count);
     return 0;
 }
