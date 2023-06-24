0e3655e6d294b9e6cb85238b0b88c88b9313a71a
Ge CHEN (Kevin) <kevin.ge.chen@gmail.com>
2023-06-24 04:09:37 +0000

    feat(cache): add lru cache data structure initialization

--- 05-Cache-Lab/src/csim.c
+++ 05-Cache-Lab/src/csim.c
@@ -23,6 +23,20 @@ typedef struct mem_access {
     struct mem_access *next;
 } mem_access_node;
 
+typedef struct cache_block_node {
+    unsigned int valid_bit;
+    unsigned long tag;
+    struct cache_block_node *prev;
+    struct cache_block_node *next;
+} cache_block;
+
+typedef struct {
+    unsigned int s;
+    unsigned int E;
+    unsigned int b;
+    cache_block *lru_block;
+} cache_set;
+
 /**
  * Parse line by line from trace_file,
  * build a memory access struct for each valid line.
@@ -42,15 +56,15 @@ mem_access_node* readMemAccessHistoryFromTraceFile(const char *file_name) {
     while (fgets(line, MAX_TRACE_LEN, in_file) != NULL) {
         // Skip lines for trace of instruction type 'I'
         if (line[0] != ' ') continue;
-        mem_access_node* current_node = (mem_access_node*) malloc(sizeof(mem_access_node));
-        memset(current_node, 0, sizeof(mem_access_node));  // Initialize addr with zeros
-        sscanf(line, " %c %[^,],%s", &current_node->type, current_node->addr, current_node->bsize);
+        mem_access_node* current = (mem_access_node*) malloc(sizeof(mem_access_node));
+        memset(current, 0, sizeof(mem_access_node));  // Initialize addr with zeros
+        sscanf(line, " %c %[^,],%s", &current->type, current->addr, current->bsize);
         if (NULL == head) {
-            head = current_node;
-            tail = current_node;
+            head = current;
+            tail = current;
         } else {
-            tail->next = current_node;
-            tail = current_node;
+            tail->next = current;
+            tail = current;
         }
     }
 
@@ -59,13 +73,66 @@ mem_access_node* readMemAccessHistoryFromTraceFile(const char *file_name) {
 }
 
 void printMemAccessList(mem_access_node *head) {
-    mem_access_node* move = head;
+    mem_access_node* current = head;
     printf("\nThe sequence of memory access:\n");
-    while (move) {
-        printf("\ttype=%c, addr=%s, bsize=%s, cb_len=%d\n", move->type, move->addr, move->bsize, move->cb_len);
-        move = move->next;
+    while (current) {
+        printf("\ttype=%c, addr=%s, bsize=%s, cb_len=%d\n",
+                current->type, current->addr, current->bsize, current->cb_len);
+        current = current->next;
     }
 }
+
+/**
+ * Function to free the memory occupied by the linked list Function
+ * to free the memory occupied by the linked list
+ */
+void freeMemAccessList(mem_access_node* head) {
+    mem_access_node* current = head;
+    while (current != NULL) {
+        mem_access_node* temp = current;
+        current = current->next;
+        free(temp);
+    }
+}
+
+/**
+ * Initialize LRU blocks by creating
+ * a doubly linked list of cache_block.
+ * Return the tail (least recently used block pointer)
+ */
+cache_block* initLruBlocks(unsigned int E) {
+    cache_block* lru_block = NULL;
+    cache_block* current = NULL;
+    for (unsigned int i = 0; i < E; i++) {
+        lru_block = (cache_block*) malloc(sizeof(cache_block));
+        memset(lru_block, 0, sizeof(cache_block));
+        if (NULL == current) {
+            current = lru_block;
+        } else {
+            current->next = lru_block;
+            lru_block->prev = current;
+            current = lru_block;
+        }
+    }
+
+    return lru_block;
+}
+
+/**
+ * Initialize LRU cache by returning an array of cache set
+ */
+cache_set* initLruCache(unsigned int S, unsigned int E) {
+    cache_set *lru_cache = (cache_set *) malloc(S * sizeof(cache_set));
+    memset(lru_cache, 0, S * sizeof(cache_set));
+    for (unsigned int i = 0; i < S; i++) {
+        lru_cache[i].E = E;
+        cache_block *lru_block = initLruBlocks(E);
+        lru_cache[i].lru_block = lru_block;
+    }
+
+    return lru_cache;
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -102,7 +169,14 @@ int main(int argc, char *argv[])
         }
     }
 
+    // parse trace file
     mem_access_node *mlist = readMemAccessHistoryFromTraceFile(trace_file);
+
+    // initiate lru_cache
+    // unsigned int S = 1 << s;
+    // cache_set* lru_cache = initLruCache(S, E);
+    // evaluate cache actions
+    // simulateCacheBehaviors(mlist, lru_cache, s, E, b);
 
     if (help_flag)
         printf("Usage: %s [-hv] -s <s> -E <E> -b <b> -t <tracefile>\n", argv[0]);
@@ -114,8 +188,7 @@ int main(int argc, char *argv[])
         printf("hit_count = %d, miss_count = %d, eviction_count = %d\n", hit_count, miss_count, eviction_count);
     }
 
-    // parse trace file
-    // evaluate cache actions
+	freeMemAccessList(mlist);
     // calculate statistics
     // printSummary(hit_count, miss_count, eviction_count);
     return 0;
