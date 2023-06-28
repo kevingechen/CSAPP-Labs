6b0d4e2fdbfff387b8674dffc1fd5614ebaffe43
Ge CHEN (Kevin) <kevin.ge.chen@gmail.com>
2023-06-28 05:21:37 +0000

    feat(cache): add help message function

--- 05-Cache-Lab/src/csim.c
+++ 05-Cache-Lab/src/csim.c
@@ -318,6 +318,21 @@ void simulateCacheBehaviors(mem_access_node* mhead,
     }
 }
 
+void printHelpMessage(const char *command) {
+    printf("Usage: %s [-hv] -s <num> -E <num> -b <num> -t <file>\n", command);
+    printf("Options:\n");
+    printf("  -h         Print this help message.\n");
+    printf("  -v         Optional verbose flag.\n");
+    printf("  -s <num>   Number of set index bits.\n");
+    printf("  -E <num>   Number of lines per set.\n");
+    printf("  -b <num>   Number of block offset bits.\n");
+    printf("  -t <file>  Trace file.\n");
+    printf("\n");
+    printf("Examples:\n");
+    printf("  linux>  ./csim-ref -s 4 -E 1 -b 4 -t traces/yi.trace\n");
+    printf("  linux>  ./csim-ref -v -s 8 -E 2 -b 4 -t traces/yi.trace\n");
+}
+
 int main(int argc, char *argv[])
 {
     int opt;
@@ -354,7 +369,7 @@ int main(int argc, char *argv[])
     }
 
     if (help_flag) {
-        printf("Usage: %s [-hv] -s <s> -E <E> -b <b> -t <tracefile>\n", argv[0]);
+        printHelpMessage(argv[0]);
         return 0;
     }
 
